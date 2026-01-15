6a42d77ce901b2fd8ef39ee453b9656750dbdc55
KeeganE456 <erdiskeegan@gmail.com>
2026-01-15 15:49:59 -0500

    Added Welcome Note

---
+++ model.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+
+class RashCNN(nn.Module):
+    def __init__(self, num_classes):
+        super(RashCNN, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128*16*16, 256),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
